1567948167556cca89bcca3da1ce470b67967487
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-23 17:07:18 -0800

    404 error page set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Route, Switch } from 'react-router-dom';
+
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail.js';
 import './App.css';
+import NotFound from './components/NotFound.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route exact path="/" component={BookList} />
+        <Route exact path="/details/:id" component={BookDetail} />
+        <Route path="*" component={NotFound} />
+      </Switch>
     </main>
   );
 }
